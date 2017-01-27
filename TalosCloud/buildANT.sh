1c674817ddaa22dbc225ec18f057ef9315253cb2
lubux <lubu@student.ethz.ch>
2017-01-27 13:59:42 +0100

    TalosCloud ant build

--- TalosCloud/buildANT.sh
+++ TalosCloud/buildANT.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ant -silent -propertyfile taloswebservice.properties -buildfile taloswebservice.xml || { echo "Ant build failed"; exit 1; }
-rm -rf out || { echo "Remove of Build failed"; exit 1; }
+ant -silent -buildfile taloscloud.xml || { echo "Ant build failed"; exit 1; }
